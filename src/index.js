a1f2de0820bf1f59cca5f4c74363d4f0acfb1aa4
Romulus <romulus.senter@yahoo.com>
2018-03-02 20:36:39 -0500

    Removed the stoere to ta different file adn restructured project

--- src/index.js
+++ src/index.js
@@ -5,24 +5,8 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import {Provider} from 'react-redux';
 import { createStore} from 'redux';
+import store from './store.js';
 
-//inital state
-const initalState = 0;
-//reducer how state changes
-const countReducer = (state= initalState, action) => {
-  switch(action.type){
-    case "INCREMENT":
-    return state + 1;
-  case "DECREMENT":
-   return state - 1;
-  default:
-  return state 
-  }
-};
-//create of the store
-const store =createStore(countReducer,
-window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-);
 
 ReactDOM.render(
 <Provider store = {store}>
