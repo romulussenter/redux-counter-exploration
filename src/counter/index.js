1de0d40ffb76401b7ae0f9177e8de50826d70446
Romulus <romulus.senter@yahoo.com>
2018-03-05 18:21:13 -0500

    Added the types file and liked it back to the index.js

--- src/counter/index.js
+++ src/counter/index.js
@@ -1,14 +1,13 @@
-
-
+import * as types from './types';
 
 //inital state
 const initalState = 0;
 //reducer how state changes
 const countReducer = (state= initalState, action) => {
   switch(action.type){
-    case "INCREMENT":
+    case types.INCREMENT:
     return state + 1;
-  case "DECREMENT":
+  case types.DECREMENT:
    return state - 1;
   default:
   return state 
